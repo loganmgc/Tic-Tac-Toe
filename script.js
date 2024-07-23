56775e10fa7bb70a0734419f429ca7694dfb71d0
Guney <guneycin@gmail.com>
2024-07-23 14:53:12 +0300

    general improvements
    variable names have been edited, repetitive codes in javascript have been collected and a new game button has been added.

--- script.js
+++ script.js
@@ -2,32 +2,25 @@ const images = [`img/o.png`, `img/x.png`];
 const mStyle = ['o', 'x'];
 let moves = ['', '', '', '', '', '', '', '', ''];
 let mCounter = 0;
+var button = document.getElementsByClassName('button');
 function mainGame(index) {
     let bgImage = document.getElementById('t' + index);
     document.getElementById('b' + index).setAttribute('hidden', true);
     bgImage.style.backgroundImage = `url('${images[1]}')`;
     moves[index] = mStyle[1];
-    let button = document.getElementsByClassName('button');
-    for (var i = 0; i < button.length; i++) {
-        button[i].setAttribute('disabled', true);
-    }
+    disableButtons(true);
     mCounter++;
     if (mCounter > 4 && determineWinner() != '0') {
-        document.getElementById('resutlmsg').style.opacity = 1;
-        document.getElementById('result').innerText = determineWinner();
+        updateResultMessage();
         return;
     }
     setTimeout(() => {
         cMove();
         if (mCounter > 4 && determineWinner() != '0') {
-            document.getElementById('resutlmsg').style.opacity = 1;
-            document.getElementById('result').innerText = determineWinner();
+            updateResultMessage();
             return;
         }
-        for (var i = 0; i < button.length; i++) {
-            button[i].disabled = false;
-        }
-
+        disableButtons(false);
     }, 1500);
 }
 function cMove() {
@@ -72,4 +65,25 @@ function checkWin() {
         }
     }
     return null;
+}
+function disableButtons(state) {
+    let buttons = document.getElementsByClassName('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = state;
+    }
+}
+function updateResultMessage() {
+    document.getElementById('result-message-container').style.opacity = 1;
+    document.getElementById('result-message').innerText = determineWinner();
+    button[9].removeAttribute('disabled');
+}
+function newGame() {
+    for (var i = 0; i < moves.length; i++) {
+        moves[i] = '';
+        document.getElementById('b' + i).removeAttribute('hidden');
+        document.getElementById('t' + i).style.backgroundImage = '';
+        button[i].disabled = false;
+    }
+    document.getElementById('result-message-container').style.opacity = 0;
+    mCounter = 0;
 }
